dc7b0a9a6189d831c60e7c150b598ff3d1d7df9e
Marcin Chlewicki <chlewicki@gmail.com>
2018-10-31 11:54:17 +0100

    Update - better filters

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ collectedText = collectedText.replace('undefined', '');
 let newStr = JSON.stringify(collectedText);
 newStr = newStr.substring(0, newStr.length-1);
 newStr = newStr.substring(1);
-
 let texty = newStr.split('\\n');
+Array.prototype.remove = function() {
+    let what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+texty = texty.remove('\\t');
+texty = texty.remove(' ');
 texty = texty.filter(Boolean);
 var keys = [];
 for (let i = 0; i < texty.length; i++){
